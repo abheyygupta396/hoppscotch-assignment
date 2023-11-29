abe1efbd38b1a86a1e1ea26d00a3db39149949ab
Abhey_AbheyGupta <abheyygupta396@gmail.com>
2023-11-30 00:34:43 +0530

    setup react app with typescript webpack babel and othe configs an initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-assignment
+
+This repo contains assigment based task.
+
+Repo Link: https://github.com/abheyygupta396/hoppscotch-assignment.git
---
+++ webpack.config.ts
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
+  },
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    filename: `[name].[contenthash]-${Date.now()}.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          cacheDirectory: true,
+          babelrc: false,
+          presets: [
+            ["@babel/preset-env", { targets: { browsers: "last 2 versions" } }],
+            "@babel/preset-typescript",
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "react-hot-loader/babel",
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
+          ],
+        },
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]",
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 8000,
+    open: true,
+    historyApiFallback: true, //! to fallback on page after reloading
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
+      template: './public/index.html',
+      minify: true,
+      filename: (entryName) => 'index.html',
+    }),
+  ],
+};
